597b09fd6201cafdf90b4b505b4f938d9212eaa2
Joseph-Makgopa <u18166793@tuks.co.za>
2024-01-28 20:33:59 +0200

    added skeleton methonds in subjectDialogController

--- src/main/java/com/example/demo/controllers/SubjectDialogController.java
+++ src/main/java/com/example/demo/controllers/SubjectDialogController.java
@@ -1,4 +1,27 @@
 package com.example.demo.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class SubjectDialogController {
+
+    @FXML
+    public void add(ActionEvent event){
+
+    }
+
+    @FXML
+    public void remove(ActionEvent event){
+
+    }
+
+    @FXML
+    public void apply(ActionEvent event){
+
+    }
+
+    @FXML
+    public void cancel(ActionEvent event){
+
+    }
 }
