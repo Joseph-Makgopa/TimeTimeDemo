adbd53a7e4b0a3030898edff802b27072c7553b4
Joseph-Makgopa <u18166793@tuks.co.za>
2024-01-28 14:36:56 +0200

    created subject dialog box and implemented showSubjectDialog method in DemoController

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -1,14 +1,34 @@
 package com.example.demo.controllers;
 
+import com.example.demo.DemoApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DemoController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void showSubjectDialog(ActionEvent event){
+        FXMLLoader fxmlLoader = new FXMLLoader(DemoApplication.class.getResource("subject-dialog-view.fxml"));
+
+        try {
+            Parent parent = fxmlLoader.load();
+            SubjectDialogController dialogController = fxmlLoader.getController();
+
+            Scene scene = new Scene(parent, 300, 200);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+        }catch(IOException error){
+            error.printStackTrace();
+        }
     }
 }
