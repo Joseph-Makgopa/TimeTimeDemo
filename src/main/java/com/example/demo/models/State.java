0602cf61db3047394581e671539c77fa2c0abf09
Joseph-Makgopa <u18166793@tuks.co.za>
2024-01-28 13:00:30 +0200

    added the containers for subjects, grades, educators and sessions in state class

--- src/main/java/com/example/demo/models/State.java
+++ src/main/java/com/example/demo/models/State.java
@@ -1,18 +1,27 @@
 package com.example.demo.models;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class State {
-    private ArrayList<Day> days;
+    private final ArrayList<Day> days;
+    private final ArrayList<Subject> subjects;
+    private final ArrayList<Grade> grades;
+    private final Map<Integer, Educator> educators;
+    private final Map<Integer, Session> sessions;
     private static State instance = null;
     private State(){
         days = new ArrayList<>();
+        subjects = new ArrayList<>();
+        grades = new ArrayList<>();
+        educators = new HashMap<>();
+        sessions = new HashMap<>();
     }
     public static State getInstance(){
         if(instance == null){
             instance = new State();
         }
-
         return instance;
     }
 }
